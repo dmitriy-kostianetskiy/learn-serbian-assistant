ae71cd5229ea42df04b9e7eb620452aa8ba828f1
Dmitry Kostianetsky <dmitry.kostianetsky@noahmediagroup.com>
2024-03-28 15:26:19 +0100

    feat: make simple OpenAI call

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log(process.env.OPEN_AI_TOKEN);
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_TOKEN,
+});
+
+async function main(): Promise<void> {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: 'Conjugate Serbian verb "dati"',
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+
+  const [firstChoice] = chatCompletion.choices;
+
+  console.log(firstChoice.message.content);
+}
+
+main();
