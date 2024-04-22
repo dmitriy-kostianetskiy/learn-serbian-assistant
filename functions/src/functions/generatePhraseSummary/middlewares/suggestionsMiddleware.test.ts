f638476cfbc6499939a7071d427951025eb68e55
Dmitry Kostianetsky <dmitry.kostianetsky@noahmediagroup.com>
2024-04-22 08:46:32 +0200

    feat: add more tests

---
+++ functions/src/functions/generatePhraseSummary/middlewares/suggestionsMiddleware.test.ts
@@ -0,0 +1,110 @@
+import { Telegram } from 'telegraf';
+import { suggestionsMiddleware } from './suggestionsMiddleware';
+import { Message } from '../../../consts/message';
+import { Context } from './context';
+import { SuggestionService } from '../../../services/suggestionService';
+import { Suggestions } from '../../../model/suggestions';
+
+describe('suggestionsMiddleware', () => {
+  const createContext = (suggestions: Suggestions): Context => {
+    const suggestionService: SuggestionService = {
+      generate: jest.fn(async () => {
+        return suggestions;
+      }),
+    };
+
+    const telegram = {
+      sendMessage: jest.fn(),
+    } as unknown as Telegram;
+
+    return {
+      dependencies: {
+        suggestionService,
+        telegram,
+      },
+      payload: {
+        userId: 0,
+        chatId: 0,
+        messageId: 0,
+        userDetails: {},
+      },
+    } as Context;
+  };
+
+  test('should pass if the phrase is in serbian', async () => {
+    // Arrange
+    const suggestions: Suggestions = {
+      language: 'serbian',
+      suggestions: [],
+    };
+
+    const context = createContext(suggestions);
+    const next = jest.fn();
+
+    // Act
+    await suggestionsMiddleware(context, next);
+
+    // Assert
+    expect(next).toHaveBeenCalled();
+
+    expect(context.dependencies.telegram.sendMessage).not.toHaveBeenCalled();
+  });
+
+  test('should not pass and reply with options if the phrase is in english', async () => {
+    // Arrange
+    const suggestions: Suggestions = {
+      language: 'english',
+      suggestions: ['dete'],
+    };
+
+    const context = createContext(suggestions);
+    const next = jest.fn();
+
+    // Act
+    await suggestionsMiddleware(context, next);
+
+    // Assert
+    expect(next).not.toHaveBeenCalled();
+
+    expect(context.dependencies.telegram.sendMessage).toHaveBeenCalledWith(
+      expect.any(Number),
+      Message.PhraseIsNotInSerbian,
+      expect.objectContaining({
+        reply_markup: {
+          reply_parameters: undefined,
+          inline_keyboard: [
+            [
+              {
+                text: 'dete',
+                callback_data: 'option-0',
+              },
+            ],
+          ],
+        },
+      }),
+    );
+  });
+
+  test('should not pass and reply with error message if no suggestions', async () => {
+    // Arrange
+    const suggestions: Suggestions = {
+      language: 'unknown',
+      suggestions: [],
+    };
+
+    const context = createContext(suggestions);
+    const next = jest.fn();
+
+    // Act
+    await suggestionsMiddleware(context, next);
+
+    // Assert
+    expect(next).not.toHaveBeenCalled();
+
+    expect(context.dependencies.telegram.sendMessage).toHaveBeenCalledWith(
+      expect.any(Number),
+      Message.PhraseCanNotBeInterpret,
+      expect.any(Object),
+    );
+  });
+});
