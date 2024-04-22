3cbb1f4d6884e215e0fa414a63315a6d79f78712
Dmitry Kostianetsky <dmitry.kostianetsky@noahmediagroup.com>
2024-04-22 12:34:24 +0200

    fix: secret name

--- functions/src/consts/secret.ts
+++ functions/src/consts/secret.ts
@@ -2,6 +2,6 @@ import { defineSecret } from 'firebase-functions/params';
 
 export const Secret = {
   OpenAiKey: defineSecret('OPEN_AI_KEY'),
-  TelegramBotToken: defineSecret('OPEN_AI_KEY'),
+  TelegramBotToken: defineSecret('TELEGRAM_BOT_TOKEN'),
   SecretToken: defineSecret('SECRET_TOKEN'),
 } as const;
